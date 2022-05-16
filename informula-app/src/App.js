1e743d0a95d8b374a6b12c71753c4ae56f46dd13
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-05-16 16:48:19 -0400

    Continue adding data

--- informula-app/src/App.js
+++ informula-app/src/App.js
@@ -40,7 +40,7 @@ const App = () => {
   return (
     <div className="App">
       <div className='products'>
-        
+        <Product />
       </div>
     </div>
   );
--- informula-app/src/Product.js
+++ informula-app/src/Product.js
@@ -1,11 +1,23 @@
 import React, {useState} from 'react';
+import {formulaData} from '../src/data';
 //import ProductDetails from 'ProductDetails';
 
 const Product = (Title, Price, Rating, Reviews, Store, Link, Image) => {
     return (
+        <>
         <div>
-            <h1>Title</h1>
+            <div className='product-container'>
+                {formulaData.map((data, key) => {
+                    return (
+                        <div key={key} className="product-card">
+                            <div className='product-title'>{data.Title}</div>
+                            <div className='product-price'>{data.Price}</div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
+        </>
     )
 }
 
