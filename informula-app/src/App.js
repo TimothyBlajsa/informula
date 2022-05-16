40b7fa0d70d31d448a162c03858897e22be1cc8d
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-05-16 16:17:54 -0400

    Add local data

--- informula-app/src/App.js
+++ informula-app/src/App.js
@@ -1,23 +1,47 @@
+/* eslint-disable array-callback-return */
 import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Product from '../src/Product';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // const APP_ID = '4frcyZz6YFdG1JTIZLYb';
+  // const APP_KEY = '1zoH35JLKD98pgRiudMMgwa48EFzSjXs';
+
+  // const [products, setProducts]= useState();
+  // const exampleRequest = `https://simplescraper.io/api/${APP_ID}?apikey=${APP_KEY}&limit=100`;
+
+  // //const [counter, setCounter] = useState(0);
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+  // const getProducts = async () => {
+  //   const response = await axios.get(exampleRequest);
+  //   setProducts(response.data.data);
+    
+  //   const productData = [];
+  //   const range = await response.data.data.to - response.data.data.from;
+  //   for (let i = 0; i < range; i++) {
+  //     productData.push({
+  //       "img": response.data.hits[i].data.Image,
+  //       "title": response.data.hits[i].data.Title
+  //     });
+  //   }
+    
+    
+  // }
+  // console.log(products)
+  // //console.log(products.map(a => a.Price))
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='products'>
+        
+      </div>
     </div>
   );
 }
---
+++ informula-app/src/Product.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+//import ProductDetails from 'ProductDetails';
+
+const Product = (Title, Price, Rating, Reviews, Store, Link, Image) => {
+    return (
+        <div>
+            <h1>Title</h1>
+        </div>
+    )
+}
+
+export default Product;
